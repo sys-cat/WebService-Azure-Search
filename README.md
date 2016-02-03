c705adf490e7c04ecd69e646aa7854b4beefdb3d
sys_cat <systemcat91@gmail.com>
2016-02-03 16:18:03 +0900

    add cpan module.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Azure::Search - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Azure::Search;
+
+# DESCRIPTION
+
+WebService::Azure::Search is ...
+
+# LICENSE
+
+Copyright (C) sys\_cat.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sys\_cat &lt;systemcat91@gmail.com>
---
+++ lib/WebService/Azure/Search.pm
@@ -0,0 +1,39 @@
+package WebService::Azure::Search;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Azure::Search - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Azure::Search;
+
+=head1 DESCRIPTION
+
+WebService::Azure::Search is ...
+
+=head1 LICENSE
+
+Copyright (C) sys_cat.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sys_cat E<lt>systemcat91@gmail.comE<gt>
+
+=cut
+
