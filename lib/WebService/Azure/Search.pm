b9e8020f6eb5ea8ecb62f36be4d053acba5a04d2
sys_cat <systemcat91@gmail.com>
2016-02-04 20:42:01 +0900

    modify class

--- lib/WebService/Azure/Search.pm
+++ lib/WebService/Azure/Search.pm
@@ -4,15 +4,16 @@ use strict;
 use warnings;
 
 use Class::Accessor::Lite (
-  new => 1,
-  rw => [qw/_init update delete/]
-  ro => [qw/select/]
+  new => 0,
+  rw => [qw/_init query/]
 );
 
 use JSON:
 use HTTP::Request;
 use HTTP::Headers;
 use URI;
+use Try::Tiny;
+use Carp;
 
 our $VERSION = "0.01";
 
@@ -23,15 +24,43 @@ sub new {
 }
 
 sub _init {
+  my ($self) = @_;
+  $self->{setting}{base} = undef;
+  if ($self->{service}) {
+    $self->{setting}{base} = sprintf("https://%s.search.windows.net/", $self->{service});
+  }
+
+  $self->{setting}{index} = undef;
+  if ($self->{index}) {
+    $self->{setting}{index} = $self->{index};
+  }
+
+  $self->{setting}{api} = undef;
+  if ($self->{api}) {
+    $self->{setting}{api} = $self->{api};
+  }
+
+  $self->{setting}{admin} = undef;
+  if ($self->{admin}) {
+    $self->{setting}{admin} = $self->{admin};
+  }
+
+  try {
+    $self->{params}{url} = sprintf(
+      "%s/indexes/%s/docs/index?api-version=%s",
+      $self->{setting}{base},
+      $self->{setting}{index},
+      $self->{setting}{api}
+    );
+  } catch {
+    carp "can't create request url.detail : $_";
+  };
 }
-
-sub _query {}
-
-sub select {}
-
-sub update {}
-
-sub delete {}
+# select, insert, update, delete　のメソッドはパラメーターを作るだけ
+# 最終的なリクエストは run に任せる
+# run はHTTPリクエストして返すだけ
+sub query {
+}
 
 1;
 __END__
@@ -45,6 +74,7 @@ WebService::Azure::Search - It's new $module
 =head1 SYNOPSIS
 
     use WebService::Azure::Search;
+    my $azure = WebServise::Azure::Search->new({});
 
 =head1 DESCRIPTION
 
